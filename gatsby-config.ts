ad36692db2b11863ba94f6b657d19c8322069d58
kamilkodzi <bizkamiladamski@gmail.com>
2022-05-24 20:14:00 +0200

    initial commit

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `gaming-platform`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
+
+export default config;
